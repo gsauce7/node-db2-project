6d3d58de2a6d88d1ba127d27f026d8b1818cf3db
Gabriel Henton <gabehenton@gmail.com>
2021-06-20 17:06:15 -0700

    middleware done

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,62 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const Car = require("./cars-model");
+
+const checkCarId = async (req, res, next) => {
+
+  try {
+    const { id } = req.params;
+    const car = await Car.getById(id);
+    if (!car) {
+      res.status(404).json({ message: `car with id ${id} is not found` });
+    } else {
+      req.car = car;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+  const { vin, make, model, mileage } = req.body;
+  const field_name = (vin || make || model || mileage);
+  if (field_name === undefined) {
+    res.status(400).json({ message: `${field_name} is missing` });
+  } else if (typeof vin !== "string") {
+    res.status(400).json({ message: `${vin} must be a string` });
+  } else if (typeof make !== "string") {
+    res.status(400).json({ message: `${make} must be a string` });
+  } else if (typeof model !== "string") {
+    res.status(400).json({ message: `${model} must be a string` });
+  } else if (typeof mileage !== "number") {
+    res.status(400).json({ message: `${mileage} must be a number` });
+  } else {
+    next();
+  }
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
+  const { vin } = req.body;
+  if (vin.trim().length !== 17) {
+    res.status(400).json({ message: `vin ${vin} is invalid. It must be exactly 17 characters in length` });
+
+  }
+};
+
+const checkVinNumberUnique = async (req, res, next) => {
+  // DO YOUR MAGIC
+  const allCars = await Car.getAll()
+  if (allCars.some(car => car.vin == req.body.vin.trim() && car.id != req.params.id)) {
+    return res.status(400).json({ message: `vin ${vin} already exists` })
+  }
+  next()
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,55 @@
 // DO YOUR MAGIC
+
+const express = require('express');
+const db = require("../../data/dbConfig.js")
+// lines 4 through 12 are commented out to give example of what NOT to do (db stuff should not be here)
+// const knex = require('knex');
+
+// const db = knex({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: './data/produce.db3'
+//   },
+//   useNullAsDefault: true
+// });
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db('cars')
+        .then(cars => {
+            res.json(cars);
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message });
+        });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('cars').where({ id }).first()
+        .then(car => {
+            res.json(car);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to retrieve car' });
+        });
+});
+
+// router.post('/', (req, res) => {
+//     const carData = req.body;
+//     db('cars').insert(carData)
+//         .then(ids => {
+//             return db('fruits').where({ id: ids[0] })
+//         })
+//         .then(newCarEntry => {
+//             res.status(201).json(newCarEntry);
+//         })
+//         .catch(err => {
+//             console.log('POST error', err);
+//             res.status(500).json({ message: "Failed to store data" });
+//         });
+// });
+
+module.exports = router;
